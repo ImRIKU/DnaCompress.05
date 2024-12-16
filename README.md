7d1a29ad4e0f99dadb3120fb1d3e72aa6f82db4e
ieRiku <mriganka9432@gmail.com>
2024-12-16 17:36:13 +0000

    modified jarvis3 with cpuUsage.c

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DnaCompress.05
 temporary adding.
+
+## jarvis and jarvis2 and jarvis3 repo
+
+https://github.com/cobilab/jarvis.git
+https://github.com/cobilab/jarvis2.git
+https://github.com/cobilab/jarvis3.git
+
+
+# To compile go to the jarvis/src folder and run
+make
+
+# Jarvis2 run
+SAMPLE                                                             
+      Run Compression   -> ./JARVIS2 -v -l 30 sequence.txt          
+      Run Decompression -> ./JARVIS2 -v -d sequence.txt.jc   
+
+# Jarvis3 run
+SAMPLE                                                             
+      Run Compression   -> ./JARVIS3 -v -l 14 sequence.txt         
+      Run Decompression -> ./JARVIS3 -v -d sequence.txt.jc 
--- jarvis3/src/Jarvis3.c
+++ jarvis3/src/Jarvis3.c
@@ -13,6 +13,8 @@
 #include <ctype.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
 
 #include "defs.h"
 #include "common.h"
@@ -31,6 +33,39 @@
 #include "bitio.h"
 #include "arith.h"
 #include "arith_aux.h"
+
+
+///////////////////////////////////////////////////////////
+/////////////////////// RAM USAGE /////////////////////////
+
+volatile bool keep_running = true;
+
+uint64_t mem_total, mem_free_beg, mem_free_end, mem_used;
+uint64_t cpu_avg, ram_avg, ram_total;
+
+extern void* get_cpu_usage(void* arg);
+
+void get_memory_usage(uint64_t* total, uint64_t* free) {
+    FILE* file = fopen("/proc/meminfo", "r");
+    if (!file) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        if (sscanf(buffer, "MemTotal: %ld kB", total) == 1 ||
+            sscanf(buffer, "MemFree: %ld kB", free) == 1) {
+            // Do nothing, just parsing
+        }
+    }
+
+    fclose(file);
+}
+
+//////////////////////////////////////////////////////////
+
+
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // FIND DATA TYPE
@@ -1231,6 +1266,22 @@ int main(int argc, char **argv)
   
   P = (PARAM *) Calloc(1, sizeof(PARAM));
 
+  ////////////////////////////////////////////////
+  /////////// CPU AND MEM USAGE //////////////////
+
+  pthread_t monitor_thread;
+  uint64_t pid = (uint64_t)getpid();
+
+  // Create a thread to monitor CPU usage
+  pthread_create(&monitor_thread, NULL, get_cpu_usage, &pid);
+
+  //////////////////////////////////////////
+  /////////   MEM USAGE CALCULATE //////////
+
+  get_memory_usage(&mem_total, &mem_free_beg);
+
+  /////////////////////////////////////////
+
   if((P->help = ArgState(DEFAULT_HELP, p, argc, "-h", "--help")) == 1 || 
   argc < 2){
     PrintMenu();
@@ -1374,6 +1425,26 @@ int main(int argc, char **argv)
   if(P->verbose)
     fprintf(stderr, "Spent %g seconds.              \n", 
     ((double)(stop-start)) / CLOCKS_PER_SEC); 
+
+
+  ////////////////////////////////////////////////
+  /////////// CPU AND MEM USAGE //////////////////
+  keep_running = false;
+
+  // Wait for the monitoring thread to finish
+  pthread_join(monitor_thread, NULL);
+
+
+  get_memory_usage(&mem_total, &mem_free_end);
+  if(mem_free_beg > mem_free_end)
+    mem_used = mem_free_beg - mem_free_end;
+  ram_total = (uint64_t)(mem_total/1000);
+  if(ram_avg == 0) ram_avg = 1;
+  fprintf(stdout,"Memory used: %"PRIu64" kb out of %"PRIu64" kb \n", mem_used, mem_total);
+	fprintf(stdout,"CPU usage: %"PRIu64" %%\n", cpu_avg);
+	fprintf(stdout,"RAM usage: %"PRIu64" mb out of %"PRIu64" mb\n", ram_avg*ram_total/100, ram_total);
+
+  ////////////////////////////////////////////////
 
   return 0;
   }
